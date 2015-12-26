6023b51a9009fff477e9da5ac3f9ece82b7c9883
Gina M Holden <ginamholden@unknown-4c:8d:79:ef:7b:1a.home>
2015-12-26 22:03:34 +0000

    first half of tap me game

--- Tap Me/ViewController.m
+++ Tap Me/ViewController.m
@@ -18,16 +18,51 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    [self setupGame];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+    
+}
+
+- (void)subtractTime{
+    seconds--;
+    timerLabel.text = [NSString stringWithFormat:@"Time: %li",seconds];
+    if(seconds==0){
+        [timer invalidate];
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Time is up!"
+                                                        message:[NSString stringWithFormat:@"You scored %li points", count]
+                                                       delegate:self
+                                              cancelButtonTitle:@"PlayAgain"
+                                              otherButtonTitles:nil];
+        [alert show];
+        
+    }
+}
+
+- (void)setupGame {
+    seconds=30;
+    count=0;
+    timerLabel.text = [NSString stringWithFormat:@"Time:%li",seconds];
+    scoreLabel.text = [NSString stringWithFormat:@"Score\n%li", count];
+    //NSTimer object. will send you a message every second.
+    timer = [NSTimer scheduledTimerWithTimeInterval:1.0f
+                                             target:self
+                                           selector:@selector(subtractTime)
+                                           userInfo:nil
+                                            repeats:YES];
 }
 
 - (IBAction)buttonPressed {
-    NSLog(@"Pressed!");
+    count++;
+    scoreLabel.text = [NSString stringWithFormat:@"Score\n%li", count];
+}
+
+- (void)alertView:(UIAlertView*)alertView clickedButtonAtIndex:(NSInteger)buttonINdex {
+    [self setupGame];
 }
 
 @end
